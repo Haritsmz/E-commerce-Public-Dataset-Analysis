e1f4a71cc19b2e233eb8dd2bd6639403f8e24ecf
Harits Mughni Zakinu <124342457+Haritsmz@users.noreply.github.com>
2024-12-12 21:34:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -28,7 +28,7 @@ with st.sidebar:
     
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
-        label='Rentang Waktu', min_value=min_date,
+        label='Time Range', min_value=min_date,
         max_value=max_date,
         value=[min_date, max_date]
     )
